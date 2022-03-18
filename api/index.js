eed1669bd4bdfbb2d3d9a91f8f5e2a683b802cb2
tatimunizz <tati.muniz0@gmail.com>
2022-03-18 09:21:49 -0300

    Updating docker

--- api/index.js
+++ api/index.js
@@ -5,12 +5,13 @@ import router from '../api/routes/index.js';
 import '../config/database/databaseConnection.js';
 
 const PORT = process.env.PORT;
+const PASSWORD = process.env.PASSWORD;
 const server = express();
 
 server.use(morgan('tiny'));
 server.use(express.json());
 server.use(basicAuth({
-  users: {'admin': 'secret'}
+  users: { 'admin': PASSWORD }
 }));
 
 server.use('/', router);
